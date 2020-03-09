c665b51540010baaad535c121bee36c4483ce393
bamchoh <bamchoh@gmail.com>
2020-03-09 22:04:58 +0900

    Turn to single instance

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/koron/go-dproxy v1.3.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/marcsauter/single v0.0.0-20191217065835-82e78df7afac
 	go.etcd.io/bbolt v1.3.3
 	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073 // indirect
 	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -11,12 +13,28 @@ import (
 	bolt "go.etcd.io/bbolt"
 
 	"test_httpserver/handler"
+
+	"github.com/marcsauter/single"
 )
 
 var db *bolt.DB
 var rootName []byte
 
 func initDB(dbfile string) (err error) {
+	if len(dbfile) == 0 {
+		return fmt.Errorf("dbfile is empty")
+	}
+
+	if !filepath.IsAbs(dbfile) {
+		execpath, err := os.Executable()
+		if err != nil {
+			return err
+		}
+		dbfile, err = filepath.Abs(filepath.Join(filepath.Dir(execpath), dbfile))
+		if err != nil {
+			return err
+		}
+	}
 	db, err = bolt.Open(dbfile, 0666, nil)
 	if err != nil {
 		err = fmt.Errorf("open DB error: %s", err)
@@ -36,6 +54,14 @@ func initDB(dbfile string) (err error) {
 }
 
 func main() {
+	s := single.New("jenkins-job-scheduler")
+	if err := s.CheckLock(); err != nil && err == single.ErrAlreadyRunning {
+		log.Fatal("another instance of the app is already running, exiting")
+	} else if err != nil {
+		log.Fatalf("failed to acquire exclusive app lock: %v", err)
+	}
+	defer s.TryUnlock()
+
 	dbname := flag.String("db", "", "database filename")
 	addr := flag.String("addr", ":20000", "server address")
 	bucket := flag.String("bucket", "MyBucket", "root bucket name")
